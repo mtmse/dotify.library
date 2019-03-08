a585819600fc5ad91aae58d79a11d04050f25c2e
Joel Håkansson <joel.hakansson@pm.me>
2019-03-08 15:03:04 +0100

    Add a configurable braille finalizer

--- src/org/daisy/dotify/translator/impl/DefaultBrailleFinalizer.java
+++ src/org/daisy/dotify/translator/impl/DefaultBrailleFinalizer.java
@@ -2,6 +2,14 @@ package org.daisy.dotify.translator.impl;
 
 import org.daisy.dotify.translator.BrailleFinalizer;
 
+/**
+ * Provides a default braille finalizer which maps spaces to
+ * braille space (0x2800) and hyphens to braille character
+ * 3-6 (0x2824). For a configurable version of this class
+ * see {@link ConfigurableBrailleFinalizer}.
+ * @author Joel Håkansson
+ *
+ */
 public class DefaultBrailleFinalizer implements BrailleFinalizer {
 	@Override
 	public String finalizeBraille(String input) {
