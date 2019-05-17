19cc7a31026e3a0283f9c37301268f3e00d2e619
Joel Håkansson <joel.hakansson@pm.me>
2019-05-17 14:32:09 +0200

    Change sub/sup definitions in LiblouisMarkerProcessor

--- src/org/daisy/dotify/translator/impl/liblouis/LiblouisMarkerProcessor.java
+++ src/org/daisy/dotify/translator/impl/liblouis/LiblouisMarkerProcessor.java
@@ -4,13 +4,10 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.daisy.dotify.api.translator.TextAttribute;
 import org.daisy.dotify.translator.DefaultMarkerProcessor;
 import org.daisy.dotify.translator.Marker;
 import org.daisy.dotify.translator.MarkerStyleConstants;
-import org.daisy.dotify.translator.RegexMarkerDictionary;
 import org.daisy.dotify.translator.SimpleMarkerDictionary;
-import org.daisy.dotify.translator.TextAttributeFilter;
 
 /**
  * <p>Provides marker processor definitions for the {@link LiblouisBrailleFilter}.
@@ -29,7 +26,6 @@ import org.daisy.dotify.translator.TextAttributeFilter;
  *
  */
 class LiblouisMarkerProcessor {
-	private static final String ALPHANUM_REGEX = "\\A[a-zA-Z0-9]+\\z";
 	private final DefaultMarkerProcessor sap;
 	private final Map<String, Integer> typeform;
 	
@@ -39,41 +35,7 @@ class LiblouisMarkerProcessor {
 	}
 
 	static LiblouisMarkerProcessor newInstance() {
-
-		TextAttributeFilter subnodeFilter = new TextAttributeFilter() {
-
-			private boolean checkChildren(TextAttribute atts) {
-				if (atts.hasChildren()) {
-					for (TextAttribute t : atts) {
-						if (t.getDictionaryIdentifier() != null) {
-							return false;
-						} else {
-							if (!checkChildren(t)) {
-								return false;
-							}
-						}
-					}
-				}
-				return true;
-			}
-
-			@Override
-			public boolean appliesTo(TextAttribute atts) {
-				return checkChildren(atts);
-			}
-		};
-		RegexMarkerDictionary sub = new RegexMarkerDictionary.Builder().
-				addPattern(ALPHANUM_REGEX, new Marker("\u2823", "")).
-				filter(subnodeFilter).
-				build();
-
-		// Svenska skrivregler för punktskrift 2009, page 32
-		RegexMarkerDictionary sup = new RegexMarkerDictionary.Builder().
-				addPattern(ALPHANUM_REGEX, new Marker("\u282c", "")).
-				filter(subnodeFilter).
-				build();
 		
-		// Redigering och avskrivning, page 148
 		SimpleMarkerDictionary dd = new SimpleMarkerDictionary(new Marker("\u2820\u2804\u2800", ""));
 		
 		SimpleMarkerDictionary continuedTableCell = new SimpleMarkerDictionary(new Marker("\u283b\u283b", ""));
@@ -83,8 +45,8 @@ class LiblouisMarkerProcessor {
 				// They are defined here only to suppress warnings from the DefaultMarkerProcessor
 				addDictionary(MarkerStyleConstants.STRONG, new SimpleMarkerDictionary(new Marker("", ""))).
 				addDictionary(MarkerStyleConstants.EM, new SimpleMarkerDictionary(new Marker("", ""))).
-				addDictionary(MarkerStyleConstants.SUB, sub).
-				addDictionary(MarkerStyleConstants.SUP, sup).
+				addDictionary(MarkerStyleConstants.SUB, new SimpleMarkerDictionary(new Marker("", ""))).
+				addDictionary(MarkerStyleConstants.SUP, new SimpleMarkerDictionary(new Marker("", ""))).
 				addDictionary(MarkerStyleConstants.DD, dd).
 				addDictionary("table-cell-continued", continuedTableCell).
 				build();
