a62aac639a9aa030b8bed381f266d9447a83ebab
Daniel Persson <mailto.woden@gmail.com>
2021-03-05 08:26:37 +0100

    Fixes after rebase.

--- src/org/daisy/dotify/translator/impl/sv/SwedishBrailleFilterFactory.java
+++ src/org/daisy/dotify/translator/impl/sv/SwedishBrailleFilterFactory.java
@@ -1,7 +1,5 @@
 package org.daisy.dotify.translator.impl.sv;
 
-import java.util.Optional;
-
 import org.daisy.dotify.api.hyphenator.HyphenatorFactoryMakerService;
 import org.daisy.dotify.api.translator.BrailleFilter;
 import org.daisy.dotify.api.translator.BrailleFilterFactory;
@@ -11,64 +9,72 @@ import org.daisy.dotify.translator.DefaultBrailleFilter;
 import org.daisy.dotify.translator.DefaultMarkerProcessor;
 import org.daisy.dotify.translator.impl.sv.SwedishMarkerProcessorFactory.SwedishMarkerProcessorConfigurationException;
 
+import java.util.Optional;
+
 class SwedishBrailleFilterFactory implements BrailleFilterFactory {
-	private static final String sv = "sv";
-	private static final String sv_SE = "sv-SE";
-	private final HyphenatorFactoryMakerService hyphenatorService;
+    private static final String sv = "sv";
+    private static final String sv_SE = "sv-SE";
+    private final HyphenatorFactoryMakerService hyphenatorService;
 
-	public SwedishBrailleFilterFactory(HyphenatorFactoryMakerService hyphenatorService) {
-		this.hyphenatorService = hyphenatorService;
-	}
+    public SwedishBrailleFilterFactory(HyphenatorFactoryMakerService hyphenatorService) {
+        this.hyphenatorService = hyphenatorService;
+    }
 
-	@Override
-	public BrailleFilter newFilter(String locale, String mode) throws TranslatorConfigurationException {
-		if (hyphenatorService == null) {
-			throw new SwedishFilterConfigurationException("HyphenatorFactoryMakerService not set.");
-		}
-		Optional<String> loc = getSupportedLocale(locale);
-		if (loc.isPresent() && mode.equals(TranslatorType.UNCONTRACTED.toString())) {
+    @Override
+    public BrailleFilter newFilter(String locale, String mode) throws TranslatorConfigurationException {
+        if (hyphenatorService == null) {
+            throw new SwedishFilterConfigurationException("HyphenatorFactoryMakerService not set.");
+        }
+        Optional<String> loc = getSupportedLocale(locale);
+        if (loc.isPresent() && mode.equals(TranslatorType.UNCONTRACTED.toString())) {
 
-			DefaultMarkerProcessor sap;
-			try {
-				sap = new SwedishMarkerProcessorFactory().newMarkerProcessor(loc.get(), mode);
-			} catch (SwedishMarkerProcessorConfigurationException e) {
-				throw new SwedishFilterConfigurationException(e);
-			}
-			return new DefaultBrailleFilter(new SwedishBrailleFilter(loc.get(), true), loc.get(), sap, hyphenatorService);
-		} 
-		throw new SwedishFilterConfigurationException("Factory does not support " + locale + "/" + mode);
-	}
-	
-	/**
-	 * Verifies that the given locale is supported.
-	 * @param locale the locale
-	 * @return the locale with correct case, or an empty optional if the locale is not supported
-	 */
-	private static Optional<String> getSupportedLocale(String locale) {
-		if (sv.equalsIgnoreCase(locale)) {
-			return Optional.of(sv);
-		} else if (sv_SE.equalsIgnoreCase(locale)) {
-			return Optional.of(sv_SE);
-		} else {
-			return Optional.empty();
-		}
-	}
-	
-	private static class SwedishFilterConfigurationException extends TranslatorConfigurationException {
+            DefaultMarkerProcessor sap;
+            try {
+                sap = new SwedishMarkerProcessorFactory().newMarkerProcessor(loc.get(), mode);
+            } catch (SwedishMarkerProcessorConfigurationException e) {
+                throw new SwedishFilterConfigurationException(e);
+            }
+            return new DefaultBrailleFilter(
+                new SwedishBrailleFilter(loc.get(), true),
+                loc.get(),
+                sap,
+                hyphenatorService
+            );
+        }
+        throw new SwedishFilterConfigurationException("Factory does not support " + locale + "/" + mode);
+    }
 
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 5954729812690753410L;
+    /**
+     * Verifies that the given locale is supported.
+     *
+     * @param locale the locale
+     * @return the locale with correct case, or an empty optional if the locale is not supported
+     */
+    private static Optional<String> getSupportedLocale(String locale) {
+        if (sv.equalsIgnoreCase(locale)) {
+            return Optional.of(sv);
+        } else if (sv_SE.equalsIgnoreCase(locale)) {
+            return Optional.of(sv_SE);
+        } else {
+            return Optional.empty();
+        }
+    }
 
-		public SwedishFilterConfigurationException(String message) {
-			super(message);
-		}
+    private static class SwedishFilterConfigurationException extends TranslatorConfigurationException {
 
-		SwedishFilterConfigurationException(Throwable cause) {
-			super(cause);
-		}
-		
-	}
+        /**
+         *
+         */
+        private static final long serialVersionUID = 5954729812690753410L;
+
+        public SwedishFilterConfigurationException(String message) {
+            super(message);
+        }
+
+        SwedishFilterConfigurationException(Throwable cause) {
+            super(cause);
+        }
+
+    }
 
 }
--- src/org/daisy/dotify/translator/impl/sv/SwedishBrailleTranslatorFactory.java
+++ src/org/daisy/dotify/translator/impl/sv/SwedishBrailleTranslatorFactory.java
@@ -1,7 +1,5 @@
 package org.daisy.dotify.translator.impl.sv;
 
-import java.util.Optional;
-
 import org.daisy.dotify.api.hyphenator.HyphenatorFactoryMakerService;
 import org.daisy.dotify.api.translator.BrailleTranslator;
 import org.daisy.dotify.api.translator.BrailleTranslatorFactory;
@@ -14,72 +12,75 @@ import org.daisy.dotify.translator.SimpleBrailleTranslator;
 import org.daisy.dotify.translator.impl.DefaultBrailleFinalizer;
 import org.daisy.dotify.translator.impl.sv.SwedishMarkerProcessorFactory.SwedishMarkerProcessorConfigurationException;
 
+import java.util.Optional;
+
 class SwedishBrailleTranslatorFactory implements BrailleTranslatorFactory {
-	private static final String sv = "sv";
-	private static final String sv_SE = "sv-SE";
-	private final HyphenatorFactoryMakerService hyphenatorService;
+    private static final String sv = "sv";
+    private static final String sv_SE = "sv-SE";
+    private final HyphenatorFactoryMakerService hyphenatorService;
 
-	public SwedishBrailleTranslatorFactory(HyphenatorFactoryMakerService hyphenatorService) {
-		this.hyphenatorService = hyphenatorService;
-	}
+    public SwedishBrailleTranslatorFactory(HyphenatorFactoryMakerService hyphenatorService) {
+        this.hyphenatorService = hyphenatorService;
+    }
 
-	@Override
-	public BrailleTranslator newTranslator(String locale, String mode) throws TranslatorConfigurationException {
-		if (hyphenatorService == null) {
-			throw new SwedishTranslatorConfigurationException("HyphenatorFactoryMakerService not set.");
-		}
-		Optional<String> loc = getSupportedLocale(locale);
-		if (loc.isPresent() && mode.equals(TranslatorType.UNCONTRACTED.toString())) {
+    @Override
+    public BrailleTranslator newTranslator(String locale, String mode) throws TranslatorConfigurationException {
+        if (hyphenatorService == null) {
+            throw new SwedishTranslatorConfigurationException("HyphenatorFactoryMakerService not set.");
+        }
+        Optional<String> loc = getSupportedLocale(locale);
+        if (loc.isPresent() && mode.equals(TranslatorType.UNCONTRACTED.toString())) {
 
-			DefaultMarkerProcessor sap;
-			try {
-				sap = new SwedishMarkerProcessorFactory().newMarkerProcessor(loc.get(), mode);
-			} catch (SwedishMarkerProcessorConfigurationException e) {
-				throw new SwedishTranslatorConfigurationException(e);
-			}
+            DefaultMarkerProcessor sap;
+            try {
+                sap = new SwedishMarkerProcessorFactory().newMarkerProcessor(loc.get(), mode);
+            } catch (SwedishMarkerProcessorConfigurationException e) {
+                throw new SwedishTranslatorConfigurationException(e);
+            }
 
-			return new SimpleBrailleTranslator(
-					new DefaultBrailleFilter(new SwedishBrailleFilter(loc.get()), loc.get(), sap, hyphenatorService),
-					new DefaultBrailleFinalizer(), mode);
-		} else if (loc.isPresent() && mode.equals(TranslatorType.PRE_TRANSLATED.toString())) {
-			return new SimpleBrailleTranslator(
-					new PreTranslatedBrailleFilter(),
-					new DefaultBrailleFinalizer(),
-					mode);
-		}
-		throw new SwedishTranslatorConfigurationException("Factory does not support " + locale + "/" + mode);
-	}
-	
-	/**
-	 * Verifies that the given locale is supported.
-	 * @param locale the locale
-	 * @return the locale with correct case, or an empty optional if the locale is not supported
-	 */
-	private static Optional<String> getSupportedLocale(String locale) {
-		if (sv.equalsIgnoreCase(locale)) {
-			return Optional.of(sv);
-		} else if (sv_SE.equalsIgnoreCase(locale)) {
-			return Optional.of(sv_SE);
-		} else {
-			return Optional.empty();
-		}
-	}
-	
-	private class SwedishTranslatorConfigurationException extends TranslatorConfigurationException {
+            return new SimpleBrailleTranslator(
+                    new DefaultBrailleFilter(new SwedishBrailleFilter(loc.get()), loc.get(), sap, hyphenatorService),
+                    new DefaultBrailleFinalizer(), mode);
+        } else if (loc.isPresent() && mode.equals(TranslatorType.PRE_TRANSLATED.toString())) {
+            return new SimpleBrailleTranslator(
+                    new PreTranslatedBrailleFilter(),
+                    new DefaultBrailleFinalizer(),
+                    mode);
+        }
+        throw new SwedishTranslatorConfigurationException("Factory does not support " + locale + "/" + mode);
+    }
 
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 5954729812690753410L;
+    /**
+     * Verifies that the given locale is supported.
+     *
+     * @param locale the locale
+     * @return the locale with correct case, or an empty optional if the locale is not supported
+     */
+    private static Optional<String> getSupportedLocale(String locale) {
+        if (sv.equalsIgnoreCase(locale)) {
+            return Optional.of(sv);
+        } else if (sv_SE.equalsIgnoreCase(locale)) {
+            return Optional.of(sv_SE);
+        } else {
+            return Optional.empty();
+        }
+    }
 
-		public SwedishTranslatorConfigurationException(String message) {
-			super(message);
-		}
+    private class SwedishTranslatorConfigurationException extends TranslatorConfigurationException {
 
-		SwedishTranslatorConfigurationException(Throwable cause) {
-			super(cause);
-		}
-		
-	}
+        /**
+         *
+         */
+        private static final long serialVersionUID = 5954729812690753410L;
+
+        public SwedishTranslatorConfigurationException(String message) {
+            super(message);
+        }
+
+        SwedishTranslatorConfigurationException(Throwable cause) {
+            super(cause);
+        }
+
+    }
 
 }
