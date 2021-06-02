e2f00021cb1d5e181a8632b4342c702e012c179e
Daniel Persson <mailto.woden@gmail.com>
2021-06-02 08:51:19 +0200

    Change so we handle the text casing the same for both original and hyphenated text.

--- src/org/daisy/dotify/translator/impl/liblouis/LiblouisBrailleFilter.java
+++ src/org/daisy/dotify/translator/impl/liblouis/LiblouisBrailleFilter.java
@@ -255,13 +255,13 @@ class LiblouisBrailleFilter implements BrailleFilter {
                 return;
             }
             String text = toTranslate.toString();
-            String hyphText = text;
             if (!markCapitals) {
                 //TODO: toLowerCase may not always do what we want here,
                 //it depends on the lower case algorithm and the rules
                 //of the braille for that language
                 text = text.toLowerCase(Locale.ROOT);
             }
+            String hyphText = text;
             if (hyphenate) {
                 String locale = processorLocale.orElse(loc);
                 HyphenatorInterface hx = hyphenators.get(locale);
